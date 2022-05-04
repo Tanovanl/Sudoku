5c1ea6d133d4417da30fcf5de525e1567b0e673a
Tanovanl <87621760+Tanovanl@users.noreply.github.com>
2022-05-04 16:48:06 +0200

    Update solver.py
    Added comments

--- solver.py
+++ solver.py
@@ -24,6 +24,27 @@ board1 =  [
     [0, 4, 9, 2, 0, 6, 0, 0, 7]
 ]
 
+
+def valid(bo, num, pos):
+    # Checks if i is in the row
+    for i in range(len(bo[0])):
+        if pos[1] != i and bo[pos[0]][i] == num:
+            return False
+    # Checks if i is in the column
+    for i in range(len(bo)):
+        if pos[0] != i and bo[i][pos[1]] == num:
+            return False
+
+    # Checks if i is in the box
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+    for i in range(box_y * 3, box_y *3 +3):
+        for j in range(box_x * 3, box_x *3 +3):
+            if bo[i][j] == num and (i,j) != pos:
+                return False
+
+    return True
+
 def solve(bord):
     print(bord)
     find = find_empty(bord)
@@ -44,29 +65,8 @@ def solve(bord):
 
     return False
 
-
-def valid(bo, num, pos):
-
-    for i in range(len(bo[0])):
-        if pos[1] != i and bo[pos[0]][i] == num:
-            return False
-
-    for i in range(len(bo)):
-        if pos[0] != i and bo[i][pos[1]] == num:
-            return False
-
-
-    box_x = pos[1] // 3
-    box_y = pos[0] // 3
-    for i in range(box_y * 3, box_y *3 +3):
-        for j in range(box_x * 3, box_x *3 +3):
-            if bo[i][j] == num and (i,j) != pos:
-                return False
-
-    return True
-
 def print_board(bo):
-    # Prints board neatly for a freater
+    # Prints board neatly for a greater overview
     for i in range(len(bo)):
         if i % 3 == 0 and i != 0:
             print("- - - - - - - - - - - -")
